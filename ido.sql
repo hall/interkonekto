df4a9326ea8c8e48f87d0ae71aeb67c5fa955006
Bryton Hall <email@bryton.io>
2018-01-31 17:38:55 -0600

    add a test entry under 'ludar' semantiko with proper multi-entry formatting

--- ido.sql
+++ ido.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:637686aae3d3a7eec82b80271169d0ca82299b7c6b4a8f18eceb998fe71fc0d6
-size 11818870
+oid sha256:8da1d4db37707b627013635392b1f4d6e2e92ac927de10a3ec8100b3141f2d9e
+size 11819012
